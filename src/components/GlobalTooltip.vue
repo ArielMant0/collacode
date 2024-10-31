71eecf97dc05951516c9b4068b18f1af9a2355d6
Franziska Becker <franziska.becker@vis.uni-stuttgart.de>
2024-10-31 14:25:58 +0100

    added selection/highlights for bar codes and bar code for all games

--- src/components/GlobalTooltip.vue
+++ src/components/GlobalTooltip.vue
@@ -1,8 +1,7 @@
 <template>
     <div v-if="data !== null" :style="{ 'top': y+'px', 'left': x+'px' }" class="my-tooltip">
         <v-sheet class="pa-2" rounded="sm" elevation="2">
-            <div v-html="data">
-            </div>
+            <div v-html="data"></div>
         </v-sheet>
     </div>
 </template>
--- src/components/vis/BarCode.vue
+++ src/components/vis/BarCode.vue
@@ -1,5 +1,5 @@
 <template>
-    <canvas ref="el" :width="completeWidth" :height="height" @pointermove="onMove" @pointerleave="tt.hide()"></canvas>
+    <canvas ref="el" :width="completeWidth" :height="completeHeight" @click="onClick"@pointermove="onMove" @pointerleave="tt.hide()"></canvas>
 </template>
 
 <script setup>
@@ -18,6 +18,10 @@
             type: Array,
             required: true
         },
+        selected: {
+            type: Array,
+            default: () => ([])
+        },
         domain: {
             type: Array,
             required: false
@@ -25,6 +29,10 @@
         colorScale: {
             type: String,
             default: "interpolateViridis"
+        },
+        idAttr: {
+            type: String,
+            default: "id"
         },
         nameAttr: {
             type: String,
@@ -36,25 +44,30 @@
         },
         width: {
             type: Number,
-            default: 5
+            default: 6
         },
         height: {
             type: Number,
             default: 50
         },
+        highlight: {
+            type: Number,
+            default: 3
+        },
         maxValue: {
             type: Number
         }
     })
+    const emit = defineEmits(["select"])
 
     const el = ref(null)
     const completeWidth = computed(() => (props.domain ? props.domain.length : props.data.length) * props.width)
+    const completeHeight = computed(() => props.height + 2*props.highlight)
 
-    let ctx, x;
+    let ctx, x, color;
 
     function draw() {
         ctx = ctx ? ctx : el.value.getContext("2d")
-        ctx.clearRect(0, 0, completeWidth.value, props.height)
 
         if (props.data.length === 0) return;
 
@@ -62,17 +75,44 @@
             .domain(props.domain ? props.domain : d3.range(props.data.length))
             .range([0, completeWidth.value])
 
-        const color = d3.scaleSequential(d3[props.colorScale])
+        color = d3.scaleSequential(d3[props.colorScale])
             .domain([1, props.maxValue ? props.maxValue : d3.max(props.data, d => d[props.valueAttr])])
 
+        drawBars();
+    }
+
+    function drawBars() {
+        ctx.clearRect(0, 0, completeWidth.value, completeHeight.value)
         if (props.domain) {
             ctx.fillStyle = "#ddd"
-            ctx.fillRect(0, 0, completeWidth.value, props.height)
+            ctx.fillRect(0, props.highlight, completeWidth.value, props.height)
         }
 
         props.data.forEach((d, i) => {
+            if (props.selected.length > 0 && props.selected.includes(d[props.idAttr])) return;
             ctx.fillStyle = props.domain ? "black" : (d[props.valueAttr] > 0 ? color(d[props.valueAttr]) : "#ddd");
-            ctx.fillRect(x(props.domain ? d[props.valueAttr] : i), 0, x.bandwidth(), props.height);
+            ctx.fillRect(
+                x(props.domain ? d[props.idAttr] : i),
+                props.highlight,
+                x.bandwidth(),
+                props.height
+            );
+        });
+
+        props.data.forEach((d, i) => {
+            if (props.selected.length === 0 || !props.selected.includes(d[props.idAttr])) return;
+            const col = props.domain ? "red" : (d[props.valueAttr] > 0 ? color(d[props.valueAttr]) : "#ddd")
+            ctx.strokeStyle = props.domain ? col : "white";
+            ctx.fillStyle = col;
+            ctx.beginPath()
+            ctx.rect(
+                x(props.domain ? d[props.idAttr] : i),
+                0,
+                x.bandwidth(),
+                completeHeight.value
+            );
+            ctx.fill()
+            ctx.stroke()
         });
     }
 
@@ -81,7 +121,7 @@
         const [mx, my] = d3.pointer(event, document.body)
         if (props.domain) {
             const id = props.domain.at(Math.min(props.domain.length-1, Math.floor(rx / x.bandwidth())))
-            const item = props.data.find(d => d[props.valueAttr] === id)
+            const item = props.data.find(d => d[props.idAttr] === id)
             if (item) {
                 tt.show(item[props.nameAttr], mx + 10, my + 10)
             } else {
@@ -89,17 +129,33 @@
             }
         } else {
             const item = props.data.at(Math.min(props.data.length-1, Math.floor(rx / x.bandwidth())))
-            tt.show(`${item[props.nameAttr]}: ${item[props.valueAttr]}`, mx + 10, my + 10)
+            tt.show(`${item[props.valueAttr]} (${(item[props.valueAttr] / props.maxValue * 100).toFixed(2)}%) - ${item[props.nameAttr]}`, mx + 10, my + 10)
+        }
+    }
+
+    function onClick(event) {
+        const [rx, _] = d3.pointer(event, el.value)
+        if (props.domain) {
+            const id = props.domain.at(Math.min(props.domain.length-1, Math.floor(rx / x.bandwidth())))
+            const item = props.data.find(d => d[props.idAttr] === id)
+            if (item) emit("select", id)
+        } else {
+            const item = props.data.at(Math.min(props.data.length-1, Math.floor(rx / x.bandwidth())))
+            if (item[props.valueAttr] > 0) emit("select", item[props.idAttr])
         }
     }
 
     onMounted(draw)
 
+    watch(() => props.selected, drawBars, { deep: true })
+
     watch(() => ([
         props.time,
         props.width,
         props.height,
+        props.highlight,
         props.colorScale,
+        props.idAttr,
         props.nameAttr,
         props.valueAttr,
         props.maxValue
