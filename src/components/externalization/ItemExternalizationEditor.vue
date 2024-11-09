96db31432623f2badfcaf9e0f51b1fc4a482a8fa
Franziska Becker <franziska.becker@vis.uni-stuttgart.de>
2024-11-09 14:11:42 +0100

    improved externalization definition

--- src/components/externalization/ItemExternalizationEditor.vue
+++ src/components/externalization/ItemExternalizationEditor.vue
@@ -1,53 +1,40 @@
 <template>
     <div class="d-flex align-start">
         <div style="width: 100%">
-            <div style="text-align: center;" class="mb-4 text-caption">
-                <p class="mb-1">
-                    <i>externalizations are <b>interactions</b> with the game that create or modify visual representations to replace mental simulation or reduce mental load
-                    they should not be <b>entirely</b> automated, but at least partially player-driven</i>
-                </p>
-                <div>
-                    <v-btn
-                        density="comfortable"
-                        class="text-caption mb-2"
-                        rounded="sm"
-                        :prepend-icon="showExamples ? 'mdi-chevron-up' : 'mdi-chevron-down'"
-                        @click="showExamples = !showExamples"
-                        color="primary">
-                        {{ showExamples ? 'hide' : 'show' }} examples
-                    </v-btn>
-                </div>
+            <div class="mb-4 text-caption">
+                <div class="d-flex justify-space-between mb-4">
+                    <v-card width="33%" class="text-caption" color="pink-lighten-5">
+                        <v-card-title class="text-pink-lighten-2" style="text-align: center;">
+                            player input
+                        </v-card-title>
+                        <v-card-text>
+                            Players must provide some (data) input that goes into creating the externalization.
+                            That can be directly inputting information (e.g., text) or meta-information (e.g., which items should be prioritzed).
+                            Simply providing an input action like a mouse click is not sufficient, there must be a connection to problem-related data.
+                        </v-card-text>
+                    </v-card>
 
-                <div v-if="showExamples" class="d-flex">
-                    <span style="width: 33%;">
-                        <div><i>strong positive</i></div>
-                        <p>
-                            <b>Example:</b> players can freely take notes inside the game
-                        </p>
-                        <p>
-                            <b>Explanation:</b> players can use such a feature quite freely to help them offload to external memory or (physically) integrate information to support the sensemaking process
-                        </p>
-                    </span>
-                    <span style="width: 33%;">
-                        <div><i>borderline</i></div>
-                        <p>
-                            <b>Example:</b> players can click on a key to visualize additional information that improves their precision or lets them integrate different pieces of information more effectively (easier or faster)
-                        </p>
-                        <p>
-                            <b>Explanation:</b> low expressiveness together with a high level of automation makes it hard to differentiate between what is just a well-designed feature and what can be considered externalization
-                        </p>
-                    </span>
-                    <span style="width: 33%;">
-                        <div><i>strong negative</i></div>
-                        <p>
-                            <b>Example:</b> players can look up information in a quest log
-                        </p>
-                        <p>
-                            <b>Explanation:</b> while a quest log certainly helps with memory, just looking up information may not help the player, especially if they have no control over what information is actually recorded
-                        </p>
-                    </span>
+                    <v-card width="33%" class="text-caption" color="purple-lighten-5">
+                        <v-card-title class="text-purple-lighten-2" style="text-align: center;">
+                            extended lifetime
+                        </v-card-title>
+                        <v-card-text>
+                            The created externalization must live for some time (through it can be short) beyond the initial input interaction.
+                            This means that information that is only shown during a hover action does not count.
+                        </v-card-text>
+                    </v-card>
+
+                    <v-card width="33%" class="text-caption" color="indigo-lighten-5">
+                        <v-card-title class="text-indigo-lighten-2" style="text-align: center;">
+                            mental load reduction
+                        </v-card-title>
+                        <v-card-text>
+                            The created externalization should reduce the player's mental load in relation to the game's challenges.
+                            Common ways of reducing mental load is by providing external storage, allowing for simulation (e.g. blueprints) or making information and items more easily accessible.
+                        </v-card-text>
+                    </v-card>
                 </div>
-                <div>
+                <div style="text-align: center;">
                     <v-btn
                         color="secondary"
                         rounded="sm"
