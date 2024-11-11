db559cbb5b4b4576b2f3bd03663131db573d42a4
Franziska Becker <franziska.becker@vis.uni-stuttgart.de>
2024-11-11 16:38:51 +0100

    added server config

--- backend/server.py
+++ backend/server.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, Request
 from flask_cors import CORS
 
 from app import bp as main_bp
+import config
+
+class CustomRequest(Request):
+    def __init__(self, *args, **kwargs):
+        super(CustomRequest, self).__init__(*args, **kwargs)
+        self.max_form_parts = 10
 
 def create_app():
     app = Flask(__name__)
-    app.config["DEBUG"] = True
-    app.config['MAX_CONTENT_LENGTH'] = 3 * 1000 * 1000
+    app.request_class = CustomRequest
+    app.config["DEBUG"] = config.DEBUG
+    app.config['MAX_CONTENT_LENGTH'] = config.MAX_CONTENT_LENGTH
+    app.config["SECRET_KEY"] = config.SECRET_KEY
     # Register blueprints here
     app.register_blueprint(main_bp)#, url_prefix="/colladata")
     CORS(app)
